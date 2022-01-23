ba46b37f58306e9ab1e65067ce0e2a3dc5d8fce0
Diya S <diya.diyu14@gmail.com>
2022-01-24 01:54:19 +0530

    added reminder notifications

---
+++ app/src/main/java/com/example/plana/ReminderBroadcast.java
@@ -0,0 +1,24 @@
+package com.example.plana;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class ReminderBroadcast extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notifychannel")
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setAutoCancel(true)
+                .setContentTitle("Task Reminder")
+                .setContentText("Check the application to view the task")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(200, builder.build());
+    }
+}
